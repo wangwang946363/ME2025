ee36179a3f3eec7e11c11059e3f0969156e5d3b0
wangwang946363 <wangwang946363@gmail.com>
2025-09-25 13:57:12 +0800

    完成猜數字設定

--- HW4_JAVASCRIPT1/static/112201523_js_1.js
+++ HW4_JAVASCRIPT1/static/112201523_js_1.js
@@ -8,4 +8,23 @@ document.getElementById("guessBtn").addEventListener("click", function()
     userGuess = Number(userGuess);
     count++;
 
-})
+    if(isNaN(userGuess))
+    {
+        alert("請輸入數字 : ");
+        return;
+    }
+    if(userGuess > answer)
+    {
+        alert("太大了，請再試一次。")
+    }
+    else if(userGuess < answer)
+    {
+        alert("太小了，請再試一次。")
+    }
+    else
+    {
+        alert('恭喜答對!你總共猜了 ${count} 次。');
+        answer = Math.floor(Math.random() * 101);
+        count = 0;
+    }
+});
