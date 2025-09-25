633450fec44b7c164d897f2dceb159f7e2c96ce7
wangwang946363 <wangwang946363@gmail.com>
2025-09-25 13:48:33 +0800

    設定按鈕及將userGuess轉成數字

--- HW4_JAVASCRIPT1/static/112201523_js_1.js
+++ HW4_JAVASCRIPT1/static/112201523_js_1.js
@@ -1,3 +1,11 @@
 //Ans
 let answer = Math.floor(Math.random() * 101);
 let count = 0;
+
+document.getElementById("guessBtn").addEventListener("click", function() 
+{
+    let userGuess = document.getElementById("guessInput").value;
+    userGuess = Number(userGuess);
+    count++;
+
+})
