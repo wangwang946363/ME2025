74bc5da9704cd24d6d40fefe0fdb240206f39a9c
wangwang946363 <wangwang946363@gmail.com>
2025-09-25 14:19:12 +0800

    設置按鈕

--- HW4_JAVASCRIPT1/static/112201523_js_2.js
+++ HW4_JAVASCRIPT1/static/112201523_js_2.js
@@ -1,2 +1,9 @@
 document.write('<h1>簡易計算機<h1>');
 document.write('<input type = "text" id = "display" randomly><br>')
+
+let buttons = 
+[
+    '0','1','2','3','4','5','6','7','8','9',
+    '+','-','*','/','(',')',
+    'clear','='
+];
