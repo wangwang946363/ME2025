20bac9c7abf68daae09d9eb123e47f346cc3254a
wangwang946363 <wangwang946363@gmail.com>
2025-09-25 14:38:53 +0800

    完成計算機

--- HW4_JAVASCRIPT1/static/112201523_js_2.js
+++ HW4_JAVASCRIPT1/static/112201523_js_2.js
@@ -10,6 +10,32 @@ let buttons =
 
 for(let i = 0; i < buttons.length; i++)
 {
-    document.write('<button onclick = "press(\'' + buttons[i] + '\')">' + buttons[i] + '<\button>');
+    document.write(`<button onclick = "press('${buttons[i]}')">${buttons[i]}</button>`);
     if((i+1)%4 === 0) document.write('<br>');
+}
+
+function press(val)
+{
+    let display = document.getElementById("display");
+    if (val === 'clear')
+    {
+        display.value = '';
+    }
+    else if(val === '=')
+    {
+        try
+        {
+            let result = eval(display.value);
+            alert(display.value + '=' + result);
+            display.value = result;
+        }
+        catch(e)
+        {
+            alert("算式錯誤。");
+        }
+    }
+    else
+    {
+        display.value += val;
+    }
 }
