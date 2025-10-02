10c18a397f37a160daf98ea25b4e93c1e48afc6c
wangwang946363 <wangwang946363@gmail.com>
2025-10-02 15:17:45 +0800

    轉整數工具

--- HW5_JAVASCRIPT2/static/112201523_js_4.js
+++ HW5_JAVASCRIPT2/static/112201523_js_4.js
@@ -3,3 +3,5 @@ const checkAll = document.getElementById("check_all");
 const totalEl = document.getElementById("total");
 const checkoutBtn = document.getElementById("ckeckout");
 const result = document.getElementById("result");
+
+const toInt = (v) => parseInt(v, 10) || 0;
