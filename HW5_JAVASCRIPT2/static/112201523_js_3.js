ddaac59eaec33f718f709eed82ca3ccbae402dff
wangwang946363 <wangwang946363@gmail.com>
2025-10-02 13:46:23 +0800

    complete the function of startGame

--- HW5_JAVASCRIPT2/static/112201523_js_3.js
+++ HW5_JAVASCRIPT2/static/112201523_js_3.js
@@ -3,3 +3,14 @@ let guessCount;
 let startTime;
 let timerInterval;
 let gameOver = false;
+
+function startGame()
+{
+    answer = Math.floor(Math.random() * 101);
+    guessCount = 0;
+    startTime = null;
+    gameOver = false;
+    document.getElementById("hint").textContent = "";
+    document.getElementById("timer").textContent = "0";
+    clearInterval(timerInterval);
+}
