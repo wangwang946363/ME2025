294b327ac9a38bb2fcae5fec0701d643373f421f
wangwang946363 <wangwang946363@gmail.com>
2025-10-02 15:16:19 +0800

    抓元素

---
+++ HW5_JAVASCRIPT2/static/112201523_js_4.js
@@ -0,0 +1,5 @@
+const table =  document.querySelector(".cart");
+const checkAll = document.getElementById("check_all");
+const totalEl = document.getElementById("total");
+const checkoutBtn = document.getElementById("ckeckout");
+const result = document.getElementById("result");
