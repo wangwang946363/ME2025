c25c709bc0f91aaaec610945c899d14a7b8dd60c
wangwang946363 <wangwang946363@gmail.com>
2025-10-02 15:27:28 +0800

    更新單列小計

--- HW5_JAVASCRIPT2/static/112201523_js_4.js
+++ HW5_JAVASCRIPT2/static/112201523_js_4.js
@@ -5,3 +5,22 @@ const checkoutBtn = document.getElementById("ckeckout");
 const result = document.getElementById("result");
 
 const toInt = (v) => parseInt(v, 10) || 0;
+
+function updateRowSubtotal(tr)
+{
+    const price = toInt(tr.querySelector(".price").textContent);
+    const qtyInput = tr.querySelector(".qty");
+    let qty = toInt(qtyInput.value);
+    //限制數量
+    const stock = toInt(tr.querySelector(".stock").textContent);
+    if (qty < 0) qty = 0;
+    if (qty >stock) qty = stock;
+    qtyInput.value = qty;
+
+    tr.querySelector(".subtotal").textContent = price * qty;
+
+    //數量=0，checkbox不能勾
+    const checkbox = tr.querySelector(".check");
+    checkbox.disabled = (stock === 0 || qty === 0);
+    if (checkbox.disabled) checkbox.checked = false;
+}
